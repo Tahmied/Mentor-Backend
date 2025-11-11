a46cb85ed816ec9bfe3e0eb296dbca07d79cd8b6
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-11-12 05:35:35 +0600

    - fixed another bug

--- Src/Routes/user.routes.js
+++ Src/Routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { loginCheck, loginUser, logoutUser, registerUser } from "../Controllers/User.controllers.js";
+import { findUser } from "../Middlewares/auth.middleware.js";
 
 const router = Router()
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
-router.post('/logout', logoutUser)
+router.post('/logout', findUser ,logoutUser)
 router.get('/logincheck', loginCheck)
 
 export default router
