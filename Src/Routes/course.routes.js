be3c4cbb552f44e64cffe1affae7ed28540baae9
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-11-12 14:41:13 +0600

    - added edit course route

--- Src/Routes/course.routes.js
+++ Src/Routes/course.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createCourse, getAllCourses, getCourseById, getMyCourses } from "../Controllers/Course.controllers.js";
+import { createCourse, getAllCourses, getCourseById, getMyCourses, updateCourse } from "../Controllers/Course.controllers.js";
 import { findUser } from "../Middlewares/auth.middleware.js";
 
 const router = Router()
@@ -8,5 +8,6 @@ router.get('/getAllCourses', getAllCourses)
 router.get('/my-courses', findUser, getMyCourses)
 router.get('/:courseId', getCourseById)
 router.post('/addCourse', findUser, createCourse)
+router.put('/updateCourse', findUser, updateCourse)
 
 export default router
